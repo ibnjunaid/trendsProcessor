06022f27156c3eebf4bedc5a0e40857c14e997e1
osama bin junaid <osama.junaid@c3m.io>
2021-09-18 14:38:28 +0530

    Implemented non nlp extracation like top hashtags, mentions, urls

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 import scraper.get_trends as scraper
+import preprocessor.pre as preprocessor
 
 tweets = scraper.retrive_tweets("#earthquake")
-scraper.save_to_db(tweets)
+
+top_urls = preprocessor.extract_top_urls(tweets)
+top_mentions = preprocessor.extract_top_mentions(tweets)
+top_hashtags = preprocessor.extract_top_hashtags(tweets)
+
+print(top_hashtags)
+#scraper.save_to_db(tweets)
--- src/scraper/get_trends.py
+++ src/scraper/get_trends.py
@@ -5,15 +5,20 @@ import db.py_mongo as py_mongo
 
 #This function scrapes a batch of 200 tweets for the given trend
 
-def retrive_tweets(trend, limit = 100, isVerified = True):
+def retrive_tweets(trend, limit = 100, isVerified = False):
     config = twint.Config()
     config.Search = trend
     config.Verified = isVerified
     config.Min_likes = 100
-    config.Lang = 'en'
-    config.Count = True
+    # config.Lang = 'en'
+    # config.Count = True
     config.Limit = limit
     config.Store_object = True
+    config.Filter_retweets = True
+    config.Hide_output = True
+    # config.Images = True
+    # config.Videos = True
+    # config.Since = str(date.today().year) + '-' + str(date.today().month) + '-' + str(date.today().day -1)
     # Initiate the search
     twint.run.Search(config)
     # Return tweets
