01d6c0425b43d3e4a2a62cb724366ab6bd3ad41c
ibnjunaid <osamabinjunaid36@gmail.com>
2023-01-31 23:08:13 +0530

    - added code to save model state - refactoring

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 from src import scrap_hashtag
+from src.analyzer.predict import predict_hashtag_sentiment
 
 
 def main(args):
@@ -18,7 +19,27 @@ def main(args):
     return {'data': scraped_data}
 
 args = {'messages': [{'key': None, 'offset': 50, 'partition': 0,
-                      'topic': 'hashtags', 'value': {"hashtags": ["JUNG HOSEOK"]}}]}
+                      'topic': 'hashtags', 'value': {"hashtags": ["#WWERaw"]}}]}
 
-x = main(args)
-print(x.get("data")[0].get("hashtag_details"))
+# x = main(args)
+# print(x.get("data")[0].get("hashtag_details"))
+
+# for k in x.get("data"):
+#     print("Predecting for ", k.get("hashtag"))
+#     y_hat = m.predict_tweet(k.get("hashtag_details"), m.freqs, m.theta)
+#     print(y_hat)
+#     if y_hat > 0.5:
+#         print('Positive sentiment')
+#     elif y_hat==0.5:
+#         print('Neutral sentiment')
+#     else: 
+#         ('Negative sentiment')
+
+y_hat = predict_hashtag_sentiment('I am feeling good')
+print(y_hat)
+if y_hat > 0.5:
+    print('Positive sentiment')
+elif y_hat==0.5:
+    print('Neutral sentiment')
+else: 
+    print('Negative sentiment')
